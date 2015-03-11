e57ffe042276f829340248f27758775851adb183
ore <kaepa3@gmail.com>
2015-03-11 13:09:13 +0000

    eekane

--- thermo_reader.py
+++ thermo_reader.py
@@ -1,10 +1,23 @@
 
 # -*- coding: utf-8 -*-
 import dhtreader
+import os
+import time
 
 type = 11
-pin = 7
+pin =26
 
+while True:
+  os.system("clear")
+  if 0 == dhtreader.init():
+    print("init error")
 
-dhtreader.init()
-print dhtreader.read(type, pin)
+  sensor_val = dhtreader.read(type, pin)
+  if sensor_val:
+    t,h = sensor_val
+    print("{0} {1}".format(t,h))
+  else:
+    print("error") 
+
+  time.sleep(3)  
+  continue
