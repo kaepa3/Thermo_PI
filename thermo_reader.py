363981681ae5ddca0efcf5c9c489a5ab54fe8368
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2015-05-28 03:07:37 +0900

    korede

--- thermo_reader.py
+++ thermo_reader.py
@@ -1,13 +1,18 @@
 
 # -*- coding: utf-8 -*-
 import dhtreader_class
-import commit_fata_class
+import commitdataclass
 
 type = 11
 pin =26
 
 reader = DhtReader_Class(type,pin)
 if True == reader.init_devise:
-   print reader.read_value
+  val = reader.read_value
+  if val != None:
+    obj = CommitDataClass()
+    obj.send_data(val.first, val.last)
+
+
 
 
